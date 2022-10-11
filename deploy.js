50983d0a0556205a5946b848fd41f642f1e3b0e5
Satyadeepsb <satyadeepsb@gmail.com>
2022-10-11 12:51:01 +0530

    Changed RPC url and private key for Window and WSL

--- deploy.js
+++ deploy.js
@@ -2,13 +2,16 @@ const ethers = require("ethers");
 const fs = require("fs-extra");
 
 async function main() {
-  // http://127.0.0.1:8545
+  // Windows - http://127.0.0.1:7545
+  // WSL - http://127.0.0.1:8545
   // connect to Ganache network using ethers.js
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:8545"
+    "http://127.0.0.1:7545"
   );
+  // Windows - a833a535f9c18c210f2366f519d5ff2ab80bf8fdf72b634d828da084fb78953a
+  // WSL - 0xfb07234f1c635d54be7415286d3595cf5ab16054b2c52931a8c059a6f52c23d5
   const wallet = new ethers.Wallet(
-    "0xfb07234f1c635d54be7415286d3595cf5ab16054b2c52931a8c059a6f52c23d5",
+    "a833a535f9c18c210f2366f519d5ff2ab80bf8fdf72b634d828da084fb78953a",
     provider
   );
   const abi = fs.readFileSync("./SimpleStoreage_sol_SimpleStorage.abi", "utf8");
